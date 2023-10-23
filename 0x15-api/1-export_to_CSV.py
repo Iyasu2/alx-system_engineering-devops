5cc69e90e44152ef86e598f460a3e55cd4bbcfb0
iyasu2 <iyasuforreal@gmail.com>
2023-10-23 18:52:47 +0300

    s

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+gather data from an API
+'''
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+
+    response = requests.get(employee_url)
+
+    employee_data = response.json()
+    employee_name = employee_data.get('name')
+
+    response = requests.get(todos_url)
+
+    todos = response.json()
+
+    filename = f"{employee_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            task_completed_status = "True" \
+                    if todo.get('completed') \
+                    else "False"
+            task_title = todo.get('title')
+            writer.writerow([employee_id,
+                             employee_name,
+                             task_completed_status,
+                             task_title])
