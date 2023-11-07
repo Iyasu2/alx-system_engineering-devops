7557674a3f430f50f63c9d59e16e59b61a3d276c
iyasu2 <iyasuforreal@gmail.com>
2023-11-07 18:36:53 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,14 +11,17 @@ def number_of_subscribers(subreddit):
     '''
     function to get no of subscribers
     '''
-    headers = {'User-Agent': 'subs'}
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+            v1.0.0 (by/u/Iyasu Asnake)'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
+    try:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
+    except Exception:
         return 0
