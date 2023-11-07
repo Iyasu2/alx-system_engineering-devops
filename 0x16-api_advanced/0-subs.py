bc380f9a1bd2a4f7d822d1131786a6c6e945b4c9
iyasu2 <iyasuforreal@gmail.com>
2023-11-07 18:39:38 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
         return 0
     headers = {'User-Agent': '0x16-api_advanced:project:\
             v1.0.0 (by/u/Iyasu Asnake)'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
