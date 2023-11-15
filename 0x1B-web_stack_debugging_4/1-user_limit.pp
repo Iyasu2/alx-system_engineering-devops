4f1f635c0a410f826b406a8da02c35093bbe878d
Iyasu2 <iyasuforreal@gmail.com>
2023-11-15 19:14:41 +0000

    s

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Increase hard file limit
+exec { 'limit-for-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit
+exec { 'soft-limit-for--user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
